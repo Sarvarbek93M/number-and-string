cbf50a4c289595a53761a9179933792f1d92664f
Sarvarbek <hamkorbank.sms@gmail.com>
2022-04-24 10:30:28 +0500

    new

---
+++ script.js
@@ -0,0 +1,31 @@
+"use script";
+const text = "Hello World";
+
+//const arr = [1, 2, 3, 4, 5, 6];
+//massive orqali keladi sonlar const da kelganda
+
+//console.log(arr.length);
+//length satr va sonlarni uzunligini ulchaydi
+
+//console.log(text[2]);
+// []  orqali cod yozilganda malumotni usha urinda turgan malumotni chiqaradi
+
+//console.log(text.toLowerCase());
+// toLowerCase() orqali malumotlarni kichik harflarda yoziladi
+
+//console.log(text.toUpperCase());
+//toUpperCase malumotlarni katta harflarda shakllantirib beradi
+
+//console.log(text.indexOf("World"));
+// indexOf("")=bu maulotni nechanchi urindan boshlanayotganini kursatadi
+
+//console.log(text.slice(7, 11));
+//slice(a, b) bu orqali belgilangan joydan malumotlarni kesib olinadi
+//console.log(text.slice(-8, -2));
+
+//console.log(text.substring(3));
+//substring (a) aning pozitsiyasidan boshlab oxirigaCHA chiqaradi
+
+console.log(text.substr(2, 11));
+//substr(a, b) a va b oraligidagi malumotlari chiqaradi
+//substr va substring da "-" orqalu kod kiritib bulmaydi
